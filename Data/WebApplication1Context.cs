565c9edaa6fc61b5b6e500b48398835405a8c3e5
francis opogah <francisopogah@gmail.com>
2022-04-04 09:44:18 +0100

    Add project files.

---
+++ Data/WebApplication1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace WebApplication1.Data
+{
+    public class WebApplication1Context : DbContext
+    {
+        public WebApplication1Context (DbContextOptions<WebApplication1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
